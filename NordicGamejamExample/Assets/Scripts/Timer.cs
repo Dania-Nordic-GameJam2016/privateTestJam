6b1d5a3cf8767e9092b725b3c7f9a0c29cfc3a89
Jonas Pedersen <jonasbjoern@gmail.com>
2016-03-05 00:39:49 +0100

    Simple timer stuff

--- NordicGamejamExample/Assets/Scripts/Timer.cs
+++ NordicGamejamExample/Assets/Scripts/Timer.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour {
 
@@ -8,6 +9,12 @@ public class Timer : MonoBehaviour {
     void Update()
     {
         timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            timer = 0;
+            GameObject.Find("Canvas/Text").GetComponent<Text>().text = "GameOver!";
+        }
     }
     void OnGUI()
     {
