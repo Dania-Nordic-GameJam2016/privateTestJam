612d3540ebda8033ab1b17bd5fa003c1fba131c9
Jonas Pedersen <jonasbjoern@gmail.com>
2016-03-04 21:57:19 +0100

    Timer added to PlayerTest scene

---
+++ NordicGamejamExample/Assets/Scripts/Timer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer : MonoBehaviour {
+
+    float timer = 10.0f;
+
+    void Update()
+    {
+        timer -= Time.deltaTime;
+    }
+    void OnGUI()
+    {
+        GUI.Box(new Rect(10, 10, 50, 20), "" + timer.ToString("0"));
+    }
+}
