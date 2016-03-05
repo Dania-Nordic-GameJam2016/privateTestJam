4eb81ca833c1045effbe7352e3207e060fb7303b
Poul Munk <pmunk87@gmail.com>
2016-03-05 04:56:36 +0100

    Pathfinding fixed  and agent spawner

---
+++ NordicGamejamExample/Assets/Scripts/Guard/AgentSpawner.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentSpawner : MonoBehaviour
+{
+    GameObject agents;
+    public GameObject agt;
+    [SerializeField]
+    private GameObject agentPrefab;
+    [SerializeField]
+    private GameObject agentElitePrefab;
+    public float timer = 30;
+    private float nextAgent;
+    int agentIndex = 0;
+    int randomModulosFactor;
+
+    void Start()
+    {
+        agents = GameObject.Find("Agents");
+    }
+
+    void Update()
+    {
+        if (Time.fixedTime > nextAgent)
+        {
+            randomModulosFactor = Random.Range(4, 8);
+            nextAgent = Time.fixedTime + timer;
+
+            if (agentIndex % randomModulosFactor == 0 && agentIndex != 0)
+            {
+                agents = Instantiate(agentElitePrefab) as GameObject;
+            }
+            else
+            {
+                agents = Instantiate(agentPrefab) as GameObject;
+            }
+
+            agents.name = "Agent " + agentIndex;
+            agentIndex++;
+            agents.transform.parent = agt.transform;
+            agents.transform.position = this.transform.position;
+        }
+    }
+
+
+}
