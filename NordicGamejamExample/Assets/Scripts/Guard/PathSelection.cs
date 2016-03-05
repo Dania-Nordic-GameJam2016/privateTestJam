9a2226a9e9051dc5e17bdc811d4b89a6fc8249a4
Poul Munk <pmunk87@gmail.com>
2016-03-05 01:15:23 +0100

    Pathfinding w0rks.

---
+++ NordicGamejamExample/Assets/Scripts/Guard/PathSelection.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PathSelection : MonoBehaviour
+{
+    private GameObject waypoints;
+    private GameObject manager;
+    private List<Transform> kids;
+    private int children;
+    public int patrolIndex = 0;
+    public int[] patrol;
+    public int[] mypath;
+    private bool pathChosen = false;
+
+    void Start()
+    {
+        patrol = new int[0];
+        kids = new List<Transform>();
+        waypoints = GameObject.Find("Waypoints");
+        manager = GameObject.Find("Game Manager");
+        children = waypoints.transform.childCount;
+        ExposeChildren();
+    }
+
+    void Update()
+    {
+        if (manager.GetComponent<WaypointList>().imReady)
+        {
+            if (pathChosen)
+            {
+                ChooseNode();
+            }
+            else
+            {
+                ChoosePatrol();
+                pathChosen = true;
+            }
+        }
+
+    }
+
+    void ExposeChildren()
+    {
+        for (int i = 0; i < children; i++)
+        {
+            kids.Add(waypoints.transform.GetChild(i));
+        }
+    }
+
+    void ChoosePatrol()
+    {
+        int random = (int)Mathf.Round(Random.Range(0, 1));
+        float distanceToChild0 = Vector3.Distance(transform.position, waypoints.transform.GetChild(0).position);
+        float distanceToChild5 = Vector3.Distance(transform.position, waypoints.transform.GetChild(5).position);
+        float distanceToChild12 = Vector3.Distance(transform.position, waypoints.transform.GetChild(12).position);
+        float distanceToChild17 = Vector3.Distance(transform.position, waypoints.transform.GetChild(17).position);
+
+        if (distanceToChild0 < distanceToChild5 && distanceToChild0 < distanceToChild12 && distanceToChild0 < distanceToChild17)
+        {
+            if (random == 0)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol1.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol1;
+            }
+            if (random == 1)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol2.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol2.Clone() as int[];
+            }
+        }
+        if (distanceToChild5 < distanceToChild0 && distanceToChild5 < distanceToChild12 && distanceToChild5 < distanceToChild17)
+        {
+            if (random == 0)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol3.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol3.Clone() as int[];
+            }
+            if (random == 1)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol4.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol4.Clone() as int[];
+            }
+        }
+        if (distanceToChild12 < distanceToChild0 && distanceToChild12 < distanceToChild5 && distanceToChild12 < distanceToChild17)
+        {
+            if (random == 0)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol5.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol5.Clone() as int[];
+            }
+            if (random == 1)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol6.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol6.Clone() as int[];
+            }
+        }
+        if (distanceToChild17 < distanceToChild0 && distanceToChild17 < distanceToChild5 && distanceToChild17 < distanceToChild12)
+        {
+            if (random == 0)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol7.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol7.Clone() as int[];
+            }
+            if (random == 1)
+            {
+                patrol = new int[manager.GetComponent<WaypointList>().patrol8.Length];
+                patrol = manager.GetComponent<WaypointList>().patrol8.Clone() as int[];
+            }
+        }
+    }
+
+    void ChooseNode()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, waypoints.transform.GetChild(patrol[patrolIndex]).position, 5 * Time.deltaTime);
+        float dist = Mathf.Sqrt(Mathf.Pow(transform.position.x - waypoints.transform.GetChild(patrol[patrolIndex]).position.x, 2) + Mathf.Pow(transform.position.z - waypoints.transform.GetChild(patrol[patrolIndex]).position.z, 2));
+        Vector3 target = waypoints.transform.GetChild(patrol[patrolIndex]).position - transform.position;
+        target.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(target);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * 7);
+
+        if (dist < 0.5f)
+        {
+            patrolIndex++;
+        }
+
+        if (patrolIndex == kids.Count - 1)
+        {
+            patrolIndex = 0;
+        }
+    }
+}
---
+++ NordicGamejamExample/Assets/Scripts/WaypointList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaypointList : MonoBehaviour
+{
+    private GameObject waypoints;
+    public bool imReady = false;
+
+    public int children;
+    public int[] patrol1;
+    public int[] patrol2; 
+    public int[] patrol3; 
+    public int[] patrol4; 
+    public int[] patrol5; 
+    public int[] patrol6; 
+    public int[] patrol7; 
+    public int[] patrol8;     
+
+    void Start()
+    {
+        patrol1 = new int[] { 0, 6, 12, 13, 14, 8, 9, 3, 2, 8, 14, 15, 16, 10, 9, 3, 2, 1, 0 };
+        patrol3 = new int[] { 5, 11, 17, 16, 15, 9, 8, 2, 3, 4, 10, 16, 15, 9, 3, 4, 5 };
+        patrol4 = new int[] { 5, 4, 3, 9, 8, 7, 13, 14, 15, 9, 10, 11, 5 };
+        patrol5 = new int[] { 12, 13, 7, 6, 0, 1, 2, 3, 9, 10, 11, 5, 4, 3, 9, 15, 14, 13, 12 };
+        patrol6 = new int[] { 12, 6, 0, 1, 2, 3, 9, 15, 16, 17, 11, 5, 4, 3, 9, 8, 2, 1, 0, 6, 12 };
+        patrol7 = new int[] { 17, 16, 15, 9, 8, 2, 1, 0, 6, 7, 13, 14, 8, 2, 3, 4, 5, 12, 17 };
+        patrol8 = new int[] { 17, 12, 11, 10, 4, 5, 6, 11, 10, 16, 15, 14, 13, 7, 6, 0, 1, 7, 8, 9, 15, 16, 17 };
+        waypoints = GameObject.Find("Waypoints");
+        children = waypoints.transform.childCount;
+        Debug.Log(patrol1.Length);
+        imReady = true;
+    }
+}
