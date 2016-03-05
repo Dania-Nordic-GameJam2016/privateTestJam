b0a9174f094e3cabd9e8639f8ab7129ea2809a4a
Mirza Tatar <mirza.tatar@gmail.com>
2016-03-05 02:15:05 +0100

    Duplicate fix lol

---
+++ NordicGamejamExample/Assets/Load.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class Load : MonoBehaviour
+{
+
+
+    private bool loadScene = false;
+
+    public bool LoadScene
+    {
+        get
+        {
+            return loadScene;
+        }
+
+        set
+        {
+            loadScene = value;
+        }
+    }
+
+    
+    // Use this for initialization
+	void Start ()
+    {
+	
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            
+            SceneManager.LoadSceneAsync("Scene6", LoadSceneMode.Additive);
+        }
+
+    }
+}
